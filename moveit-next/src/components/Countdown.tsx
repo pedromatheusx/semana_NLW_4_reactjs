a27db419976675d9e69663a845f38ff37840d86a
Pedro Matheus <pedromatheusduarte@gmail.com>
2021-02-27 20:11:49 -0300

    [aula][5]

--- moveit-next/src/components/Countdown.tsx
+++ moveit-next/src/components/Countdown.tsx
@@ -1,45 +1,87 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
+import { ChallengesContext } from "../contexts/ChallengesContext";
 import styles from "../styles/components/Countdown.module.css"
 
-export function Countdown(){
-const [time , setTime] = useState(25 * 60);
-const [active, setActive] = useState(false)
-const minutes = Math.floor(time / 60);
-const seconds = time % 60;
+let countdownTimeout: NodeJS.Timeout;
 
- const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('');
- const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('');
+export function Countdown() {
+    const {startNewChallenge} = useContext(ChallengesContext);
 
-function startCountdown(){
-setActive(true)
-}
+    const [time, setTime] = useState(0.1 * 60);
+    const [isActive, setIsActive] = useState(false)
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
 
-useEffect(() => {
-if (active && time > 0){
-    setTimeout(() => {
-        setTime(time-1)
-    }, 1000)
-}
-}, [active, time])
+    const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('');
+    const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('');
+    const [hasFinished, setHasFinished] = useState(false);
+
+    function startCountdown() {
+        setIsActive(true)
+    }
+
+    function resetCountdown() {
+        clearTimeout(countdownTimeout)
+        setIsActive(false);
+        setTime(0.1 * 60);
+    }
+
+    useEffect(() => {
+        if (isActive && time > 0) {
+            countdownTimeout = setTimeout(() => {
+                setTime(time - 1)
+            }, 1000)
+        } else if (isActive && time === 0) {
+            setHasFinished(true);
+            setIsActive(false);
+            startNewChallenge();
+        }
+    }, [isActive, time])
 
     return (
         <div>
-        <div className={styles.countdownContainer}>
-            <div>
-                <span>{minuteLeft}</span>
-                <span>{minuteRight}</span>
+            <div className={styles.countdownContainer}>
+                <div>
+                    <span>{minuteLeft}</span>
+                    <span>{minuteRight}</span>
+                </div>
+                <span>:</span>
+                <div>
+                    <span>{secondLeft}</span>
+                    <span>{secondRight}</span>
+                </div>
             </div>
-            <span>:</span>
-            <div>
-                <span>{secondLeft}</span>
-                <span>{secondRight}</span>
-            </div>
-        </div>
+            { hasFinished ? (
+                <button
+                    disabled
+                    className={styles.countdownButton}
+                >
+                    Ciclo encerrado
+                </button>
+            ) : (
+                    <>
+                        {isActive ? (
+                            <button
+                                type="button"
+                                className={`${styles.countdownButton} ${styles.countdownButtonActive}`}
+                                onClick={resetCountdown}>
+                                Abandonar ciclo
+                            </button>
+                        ) : (
+                                <button
+                                    type="button"
+                                    className={styles.countdownButton}
+                                    onClick={startCountdown}>
+                                    Iníciar um ciclo
+                                </button>
+                            )}
+                    </>
+                )}
 
 
-        <button type="button" className={styles.countdownButton} onClick={startCountdown}>
-            Iniciar um ciclo
-        </button>
+
+
+
         </div>
     );
 }
